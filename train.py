e4b35acca2e2228b744f330e3eab5c0020f24db1
Cute77 <goagoaleen@gmail.com>
2020-08-02 01:41:59 +0800

    update

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import matplotlib
 import torchvision
 from datasets import BasicDataset
 from torch.utils.data import DataLoader, random_split
+import numpy as np
 
 
 def to_var(x, requires_grad=True):
@@ -56,96 +57,88 @@ def train_net(lr=1e-3,
     val_labels = to_var(data_loader.dataset.val_label, requires_grad=False)
 
     net, opt = build_model()
-    
-    meta_losses_clean = []
-    net_losses = []
     plot_step = 100
-
-    smoothing_alpha = 0.9
-    
-    meta_l = 0
-    net_l = 0
     accuracy_log = []
-    for epoch in range(epochs)
-    for i in tqdm(range(hyperparameters['num_iterations'])):
+    for epoch in range(epochs):
         net.train()
-        # Line 2 get batch of data
-        image, labels = next(iter(data_loader))
-        # since validation data is small I just fixed them instead of building an iterator
-        # initialize a dummy network for the meta learning of the weights
-        meta_net = torchvision.models.resnet101(pretrained=True)
-        meta_net.load_state_dict(net.state_dict())
+        for i in tqdm(range(len(train))):
+            # Line 2 get batch of data
+            image, labels = next(iter(data_loader))
+            # since validation data is small I just fixed them instead of building an iterator
+            # initialize a dummy network for the meta learning of the weights
+            meta_net = torchvision.models.resnet101(pretrained=True)
+            meta_net.load_state_dict(net.state_dict())
 
-        if torch.cuda.is_available():
-            meta_net.cuda()
+            if torch.cuda.is_available():
+                meta_net.cuda()
 
-        image = to_var(image, requires_grad=False)
-        labels = to_var(labels, requires_grad=False)
+            image = to_var(image, requires_grad=False)
+            labels = to_var(labels, requires_grad=False)
 
-        # Lines 4 - 5 initial forward pass to compute the initial weighted loss
-        y_f_hat  = meta_net(image)
-        cost = F.binary_cross_entropy_with_logits(y_f_hat,labels, reduce=False)
-        eps = to_var(torch.zeros(cost.size()))
-        l_f_meta = torch.sum(cost * eps)
+            # Lines 4 - 5 initial forward pass to compute the initial weighted loss
+            y_f_hat = meta_net(image)
+            cost = F.binary_cross_entropy_with_logits(y_f_hat, labels, reduce=False)
+            eps = to_var(torch.zeros(cost.size()))
+            l_f_meta = torch.sum(cost * eps)
 
-        meta_net.zero_grad()
+            meta_net.zero_grad()
 
-                # Line 6 perform a parameter update
-        grads = torch.autograd.grad(l_f_meta, (meta_net.params()), create_graph=True)
-        meta_net.update_params(hyperparameters['lr'], source_params=grads)
-        
-        # Line 8 - 10 2nd forward pass and getting the gradients with respect to epsilon
-        y_g_hat = meta_net(val_data)
-
-        l_g_meta = F.binary_cross_entropy_with_logits(y_g_hat,val_labels)
-
-        grad_eps = torch.autograd.grad(l_g_meta, eps, only_inputs=True)[0]
-        
-        # Line 11 computing and normalizing the weights
-        w_tilde = torch.clamp(-grad_eps,min=0)
-        norm_c = torch.sum(w_tilde)
-
-        if norm_c != 0:
-            w = w_tilde / norm_c
-        else:
-            w = w_tilde
-
-        # Lines 12 - 14 computing for the loss with the computed weights
-        # and then perform a parameter update
-        y_f_hat = net(image)
-        cost = F.binary_cross_entropy_with_logits(y_f_hat, labels, reduce=False)
-        l_f = torch.sum(cost * w)
-
-        opt.zero_grad()
-        l_f.backward()
-        opt.step()
-        
-        if i % plot_step == 0:
-            net.eval()
-
-            acc = []
-            for (test_img, test_label) in enumerate(test_loader):
-                test_img = to_var(test_img, requires_grad=False)
-                test_label = to_var(test_label, requires_grad=False)
-
-                output = net(test_img)
-                predicted = (F.sigmoid(output) > 0.5).numpy()
-
-                acc.append((predicted.numpy() == test_label.numpy()).float())
-
-            accuracy = torch.cat(acc,dim=0).mean()
-            accuracy_log.append(np.array([i,accuracy])[None])
-            acc_log = np.concatenate(accuracy_log, axis=0)
+            # Line 6 perform a parameter update
+            grads = torch.autograd.grad(l_f_meta, (meta_net.params()), create_graph=True)
+            meta_net.update_params(lr, source_params=grads)
             
-        if save_cp:
-            try:
-                os.mkdir(dir_checkpoint)
-                logging.info('Created checkpoint directory')
-            except OSError:
-                pass
-            torch.save(net.state_dict(),
-                       dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
-            logging.info(f'Checkpoint {epoch + 1} saved !')
+            # Line 8 - 10 2nd forward pass and getting the gradients with respect to epsilon
+            y_g_hat = meta_net(val_data)
+
+            l_g_meta = F.binary_cross_entropy_with_logits(y_g_hat,val_labels)
+
+            grad_eps = torch.autograd.grad(l_g_meta, eps, only_inputs=True)[0]
+            
+            # Line 11 computing and normalizing the weights
+            w_tilde = torch.clamp(-grad_eps, min=0)
+            norm_c = torch.sum(w_tilde)
+
+            if norm_c != 0:
+                w = w_tilde / norm_c
+            else:
+                w = w_tilde
+
+            # Lines 12 - 14 computing for the loss with the computed weights
+            # and then perform a parameter update
+            y_f_hat = net(image)
+            cost = F.binary_cross_entropy_with_logits(y_f_hat, labels, reduce=False)
+            l_f = torch.sum(cost * w)
+
+            opt.zero_grad()
+            l_f.backward()
+            opt.step()
+            
+            if i % plot_step == 0:
+                net.eval()
+
+                acc = []
+                for (test_img, test_label) in enumerate(test_loader):
+                    test_img = to_var(test_img, requires_grad=False)
+                    test_label = to_var(test_label, requires_grad=False)
+
+                    output = net(test_img)
+                    predicted = (F.sigmoid(output) > 0.5).numpy()
+
+                    acc.append((predicted.numpy() == test_label.numpy()).float())
+
+                accuracy = torch.cat(acc,dim=0).mean()
+                accuracy_log.append(np.array([i,accuracy])[None])
+                acc_log = np.concatenate(accuracy_log, axis=0)
+                
+            if save_cp:
+                try:
+                    os.mkdir(dir_checkpoint)
+                    logging.info('Created checkpoint directory')
+                except OSError:
+                    pass
+                torch.save(net.state_dict(),
+                        dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
+                logging.info(f'Checkpoint {epoch + 1} saved !')
 
         # return accuracy
     return np.mean(acc_log[-6:-1, 1])
