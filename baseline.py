c4c121923dab43d37230f273c83739695b4ed3b8
Cute77 <goagoaleen@gmail.com>
2020-08-06 01:48:55 +0800

    update

--- baseline.py
+++ baseline.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import troch.nn as nn
+import torch.nn as nn
 from torch.autograd import Variable
 import model
 from tqdm import tqdm
