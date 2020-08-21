00d7ba7cdd7479e5fb4993d147e861ff96fc55d4
Cute77 <goagoaleen@gmail.com>
2020-08-21 17:50:27 +0800

    update

--- datasets.py
+++ datasets.py
@@ -16,13 +16,13 @@ class BasicDataset(Dataset):
             datatxt = 'ISIC_2019_Training_GroundTruth_sub_train_' + str(noise_fraction) + '.csv'
 
         if mode == 'test':
-            datatxt = 'ISIC_2019_Training_GroundTruth_sub2_test.csv'
+            datatxt = 'ISIC_2019_Training_GroundTruth_sub1250_test.csv'
 
         if mode == 'val':
             datatxt = 'ISIC_2019_Training_GroundTruth_sub_clean.csv'    
 
         if mode == 'base':
-            datatxt = 'ISIC_2019_Training_GroundTruth_sub2_train.csv'        
+            datatxt = 'ISIC_2019_Training_GroundTruth_sub5000_train.csv'        
 
         # datatxt = 'ISIC_2019_Training_GroundTruth.csv'
         # lean_datatxt = 'ISIC_2019_Training_GroundTruth_clean.csv'
@@ -40,8 +40,8 @@ class BasicDataset(Dataset):
         self.transform = transforms.Compose([ 
                transforms.RandomHorizontalFlip(),
                transforms.RandomRotation(degrees=90),
-               transforms.RandomGrayscale(p=0.1),
-               transforms.RandomResizedCrop(size=224, scale=(0.3, 1.0)), 
+               # transforms.RandomGrayscale(p=0.1),
+               # transforms.RandomResizedCrop(size=224, scale=(0.3, 1.0)), 
                # transforms.Resize([224, 224]), 
                transforms.ToTensor(), 
                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[1.0, 1.0, 1.0])
