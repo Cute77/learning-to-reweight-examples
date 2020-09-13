ec9359ed51cfa7437df862d3afdb5e106918d590
Ch4nYH <hfercxx@hotmail.com>
2020-09-14 00:17:28 +0800

    update

--- train_distributed.py
+++ train_distributed.py
@@ -187,10 +187,10 @@ def train_net(noise_fraction,
             eps = torch.ones(cost.size(), requires_grad=True).cuda(local_rank) * 1e-7
             l_f_meta = torch.sum(cost * eps)
             meta_net.zero_grad()
-            grads = torch.autograd.grad(l_f_meta, (meta_net.named_parameters()), create_graph=True, retain_graph=True)
+            grads = torch.autograd.grad(l_f_meta, (meta_net.parameters()), create_graph=True, retain_graph=True)
             # meta_net.update_params(lr, source_params=grads)
             count = 0
-            update_params(meta_net, lr, meta_net.parameters(), grads)
+            update_params(meta_net, lr, meta_net.named_parameters(), grads)
             
             y_g_hat = meta_net(val_data)
     
