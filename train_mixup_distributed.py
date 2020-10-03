e79156ea0a6a5cd3c2f759068873a383efefd49f
Cute77 <joajoaleen@gmail.com>
2020-10-03 22:54:17 +0800

    update

--- train_mixup_distributed.py
+++ train_mixup_distributed.py
@@ -224,11 +224,12 @@ def train_net(noise_fraction,
             writer.add_scalar('StepAccuracy/train', ((y_predicted.int() == labels.int()).sum().item()/labels.size(0)), global_step)
             train_iter.append((y_predicted.int() == labels.int()).sum().item())
             
+            beta.repeat(1, 9)
             prob = nn.functional.softmax(y_f_hat)
             prob = prob.cuda(local_rank)
             beta = beta.cuda(local_rank)
-            print(prob.size())
-            print(beta.size())
+            # print(prob.size())
+            # print(beta.size())
             mixup_labels = beta * mixup_labels + (1-beta) * prob
             cost = loss(mixup_labels, labels)
 
