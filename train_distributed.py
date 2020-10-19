2b1f1e9beb29fd4b35e381a57262fc4cb689e811
Cute77 <joajoaleen@gmail.com>
2020-10-19 19:18:20 +0800

    update

--- train_distributed.py
+++ train_distributed.py
@@ -198,7 +198,7 @@ def train_net(noise_fraction,
                 # Line 11 computing and normalizing the weights
 
             # w_tilde = torch.clamp(-grad_eps, min=0)
-            w_tilde = nn.functional.softmax(-grad_eps, dim=1)
+            w_tilde = nn.functional.softmax(-grad_eps)
             print('w_tilde: ', w_tilde)
             norm_c = torch.sum(w_tilde)
 
