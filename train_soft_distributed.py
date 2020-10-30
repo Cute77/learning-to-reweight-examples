6e7a7539563aeb7a0421efd73cc7b3eaddc3ed51
Cute77 <joajoaleen@gmail.com>
2020-10-30 23:09:01 +0800

    update

--- train_soft_distributed.py
+++ train_soft_distributed.py
@@ -192,8 +192,9 @@ def train_net(noise_fraction,
             labels.requires_grad = False
             
             with higher.innerloop_ctx(net, opt) as (meta_net, meta_opt):
+                print('meta_net: ', list(meta_net.parameters()))
                 # print('image: ', image)
-                y_f_hat = meta_net(image)
+                # y_f_hat = meta_net(image)
                 print('y_f_hat: ', y_f_hat)
                 cost = loss(y_f_hat, labels)
                 print('cost1: ', cost)
