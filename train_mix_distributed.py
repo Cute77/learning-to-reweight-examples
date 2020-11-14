1133a4a3c219a0f5c5db8aae0896ff64e3abe42a
Cute77 <joajoaleen@gmail.com>
2020-11-14 23:22:55 +0800

    update

--- train_mix_distributed.py
+++ train_mix_distributed.py
@@ -109,7 +109,7 @@ def train_net(noise_fraction,
 
     train_sampler = distributed.DistributedSampler(train, num_replicas=num_gpus, rank=local_rank) if is_distributed else None
 
-    data_loader = DataLoader(train, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True, sampler=train_sampler)
+    data_loader = DataLoader(train, batch_size=batch_size, shuffle=False, num_workers=8, drop_last=True , pin_memory=True, sampler=train_sampler)
     test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, num_workers=8, pin_memory=True)
     val_loader = DataLoader(val, batch_size=5, shuffle=False, num_workers=8, pin_memory=True)
     
