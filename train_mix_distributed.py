cbdcff108fd89b7f2cc2162f19b7af811deac720
Cute77 <joajoaleen@gmail.com>
2020-11-15 15:30:45 +0800

    update

--- train_mix_distributed.py
+++ train_mix_distributed.py
@@ -116,7 +116,6 @@ def train_net(noise_fraction,
     # data_loader = get_mnist_loader(hyperparameters['batch_size'], classes=[9, 4], proportion=0.995, mode="train")
     # test_loader = get_mnist_loader(hyperparameters['batch_size'], classes=[9, 4], proportion=0.5, mode="test")
 
-    '''
     val_data, val_labels, _ = next(iter(val_loader))
     if is_distributed:
         val_data = val_data.cuda(local_rank)
@@ -124,7 +123,7 @@ def train_net(noise_fraction,
     else:
         val_data = val_data.cuda()
         val_labels = val_labels.cuda()
-    '''
+    
     data = iter(data_loader)
     vali = iter(val_loader)
     loss = nn.CrossEntropyLoss(reduction="none")
@@ -190,19 +189,19 @@ def train_net(noise_fraction,
             except StopIteration:
                 data = iter(data_loader)
                 image, labels, _ = next(data)
-            
+            '''
             try:
                 val_data, val_labels, _ = next(vali)
             except StopIteration:
                 vali = iter(val_loader)
                 val_data, val_labels, _ = next(vali)                
-            
+            '''
             # meta_net.load_state_dict(net.state_dict())
             if is_distributed:
                 image = image.cuda(local_rank)
                 labels = labels.cuda(local_rank)
-                val_data = val_data.cuda(local_rank)
-                val_labels = val_labels.cuda(local_rank)
+                # val_data = val_data.cuda(local_rank)
+                # val_labels = val_labels.cuda(local_rank)
             else:
                 image = image.cuda()
                 labels = labels.cuda()
